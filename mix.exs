72155bc033a0d57c5786f1de0d2da5d78a3d9693
Nduati Kuria <012nkuria@gmail.com>
2022-10-17 11:02:59 +0200

    Fix PyCell registration

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule PyCell.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      mod: {PyCell.Application, []}
     ]
   end
 
